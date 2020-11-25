db8880eef824177f6b92dba0d99d0b40f4cb1c17
ericmartineau <smartytime@gmail.com>
2020-11-24 23:54:34 -0700

    Moving keyboard visibity

--- lib/layouts.dart
+++ lib/layouts.dart
@@ -1,4 +1,5 @@
 export 'layouts/desktop_layout.dart';
+export 'layouts/keyboard_visibility_builder.dart';
 export 'layouts/mobile_layout.dart';
 export 'layouts/responsive_constraint.dart';
 export 'layouts/sunny_page_layouts.dart';
---
+++ lib/layouts/keyboard_visibility_builder.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sunny_core_widgets/container/spaced.dart';
+
+typedef KeyboardVisibilityBuilder = Widget Function(
+    BuildContext context, Widget child, bool isKeyboardVisible);
+
+/// Calls `builder` on keyboard close/open.
+/// https://stackoverflow.com/a/63241409/1321917
+class KeyboardVisibility extends StatefulWidget {
+  final Widget child;
+  final KeyboardVisibilityBuilder builder;
+
+  const KeyboardVisibility.builder({
+    Key key,
+    this.child,
+    @required this.builder,
+  }) : super(key: key);
+
+  KeyboardVisibility({Key key, Widget onKeyboardShow, Widget onKeyboardHide})
+      : child = null,
+        builder = ((_, __, bool isShown) => isShown
+            ? (onKeyboardShow ?? emptyBox)
+            : (onKeyboardHide ?? emptyBox)),
+        super(key: key);
+
+  @override
+  _KeyboardVisibilityState createState() => _KeyboardVisibilityState();
+}
+
+class _KeyboardVisibilityState extends State<KeyboardVisibility>
+    with WidgetsBindingObserver {
+  var _isKeyboardVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeMetrics() {
+    final bottomInset = WidgetsBinding.instance.window.viewInsets.bottom;
+    final newValue = bottomInset > 0.0;
+    if (newValue != _isKeyboardVisible) {
+      setState(() {
+        _isKeyboardVisible = newValue;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.builder(
+        context,
+        widget.child,
+        _isKeyboardVisible,
+      );
+}
